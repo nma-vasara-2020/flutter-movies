e64c89eb8a2bad7eff6c207d99762ee7c641e49a
Karolis Vycius <k.vycius@gmail.com>
2020-08-14 18:11:49 +0300

    Task 2 code removal

--- lib/ui/about_page.dart
+++ lib/ui/about_page.dart
@@ -19,44 +19,11 @@ class AboutPageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: Task 2
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset(
-            "assets/nma-logo.png",
-            width: 250,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            "National Student Academy (Nacionalinė moksleivių akademija, or NMA) is a unique institution of supplementary education intended for gifted Lithuanian children to promote their talents in academic range and music. More than 300 most talented children from different parts of Lithuania study in this Academy functioning for the tenth year.",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 16),
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton.icon(
-                onPressed: _launchWebsite,
-                icon: Icon(Icons.public),
-                label: Text("Website"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                onPressed: _launchFacebook,
-                child: Text("Facebook"),
-              ),
-            ),
-          ],
-        ),
+        Text("TODO: Task 2"),
       ],
     );
   }
